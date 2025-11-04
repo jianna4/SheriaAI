0791d5bafbd7e515fbf18e42e6d3cd095606bef3
Joan Maina <mainajoan555@gmail.com>
2025-11-04 17:25:40 +0300

    docs

--- project/app/vectorstore.py
+++ project/app/vectorstore.py
@@ -11,12 +11,16 @@ from langchain.schema import Document
 with open(r"F:\Program Files\projects\sheria_AI\Sheria_backend\project\app\kenya_employment_act_chunks.json", "r", encoding="utf-8") as f:
     data = json.load(f)
 
-# Convert back to Document objects
-documents = [Document(page_content=d["page_content"], metadata=d["metadata"]) for d in data]
+
+
+
+# Convert to LangChain Document objects
+documents = [Document(page_content=item["page_content"], metadata=item["metadata"]) for item in data]
+
+print(f"Loaded {len(documents)} documents with metadata ready for embedding.")
+print("Example metadata:", documents[0].metadata)
 
 embeddings = OllamaEmbeddings(model="nomic-embed-text")
-
-
 #now the vector db
 vectorstore = FAISS.from_documents(documents, embeddings)
 print("Vector store created with FAISS.")
