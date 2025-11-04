d5f1b179a1aee09cd57ec3da08ea6d6a51d89fab
Joan Maina <mainajoan555@gmail.com>
2025-11-04 18:45:48 +0300

    views endpoiint

--- project/app/views.py
+++ project/app/views.py
@@ -1,3 +1,25 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+from .chatbot.retrieval import quey_vectorstore
+import json
+
+@csrf_exempt
+def chatbot(request):
+    if request.method == "POST":
+        try:
+            body = json.loads(request.body.decode("utf-8"))
+            query = body.get("query")
+
+            if not query:
+                return JsonResponse({"error": "Missing 'query' in request body"}, status=400)
+
+            answer = quey_vectorstore(query)
+            return JsonResponse({"query": query, "answer": answer}, status=200)
+
+        except Exception as e:
+            return JsonResponse({"error": str(e)}, status=500)
+
+    return JsonResponse({"error": "Only POST method allowed"}, status=405)
