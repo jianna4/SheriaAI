eaa18966f81ad77b7bc5f239f49662a364c24dea
Joan Maina <mainajoan555@gmail.com>
2026-05-03 16:28:51 +0300

    embeddedchunks

--- Backend/chunking.py
+++ Backend/chunking.py
@@ -7,9 +7,9 @@ import json
 from typing import List
 from dataclasses import dataclass, asdict
 from langchain_community.document_loaders import PyMuPDFLoader
-from langchain.text_splitter import RecursiveCharacterTextSplitter
-from langchain.schema import Document
 
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain_core.documents import Document
 @dataclass
 class Chunk:
     """Represents a single chunk for storage"""
